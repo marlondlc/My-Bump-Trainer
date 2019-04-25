f3f08f38abc6f6fcd2a43e79a2bcfd85d8c77902
Marlon <m.de.la.cruz@hotmail.com>
2019-04-25 23:29:56 +0000

    typo on water_intake to water_entries

--- back_end/db/migrate/20190425184144_create_food_entries.rb
+++ back_end/db/migrate/20190425184144_create_food_entries.rb
@@ -3,7 +3,7 @@ class CreateFoodEntries < ActiveRecord::Migration[5.2]
     create_table :food_entries do |t|
       t.integer :serving_size
       t.datetime :ate_at
-      t.references :users_pregnant_ladies, foreign_key: true, index:true
+      t.references :users_pregnant_ladies, :food_options, foreign_key: true, index:true
       t.timestamps
     end
   end
