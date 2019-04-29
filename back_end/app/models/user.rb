bf0c288e87b5b4bf6497ef599f2bc6a2d52fcd62
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-04-29 19:02:16 +0000

    fixed conflict for user

--- back_end/app/models/user.rb
+++ back_end/app/models/user.rb
@@ -1,14 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-    has_many :weight_entries
-    has_many :water_entries
-    has_many :food_entries
-    has_many :exercise_entries
-  
-    has_many :food_options, through: :food_entries
-    has_many :exercise_options, through: :exercise_entries
-  end
-=======
   has_many :weight_entries
   has_many :water_entries
   has_many :food_entries
@@ -17,4 +7,3 @@ class User < ApplicationRecord
   has_many :food_options, through: :food_entries
   has_many :exercise_options, through: :exercise_entries
  end
->>>>>>> d795354afc81f7724da3b06217473b97aee4ddfa
