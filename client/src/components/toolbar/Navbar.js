2d154ddc556790dfc25d525f87d8f466180b34eb
Marlon <m.de.la.cruz@hotmail.com>
2019-05-07 15:29:35 +0000

    issue

--- client/src/components/toolbar/Navbar.js
+++ client/src/components/toolbar/Navbar.js
@@ -36,20 +36,16 @@ const styles = {
 //   );
 // }
 
-
-
 class MenuAppBar extends React.Component {
   state = {
     auth: true,
     anchorEl: null,
     funfact: {
-      id:5,
-      email:'',
-      name:''
+      id: 5,
+      email: "",
+      name: ""
     }
   };
-
-
 
   handleMenu = event => {
     this.setState({ anchorEl: event.currentTarget });
@@ -59,14 +55,15 @@ class MenuAppBar extends React.Component {
     this.setState({ anchorEl: null });
   };
 
-
   render() {
     const { classes } = this.props;
     const { auth } = this.state;
 
     // Hardcoded funfact, to be replaced with below ternary fct which finds different elements for specific user logged in
-    const funfactsUser1 = "Your baby is 8 weeks old. It's the size of a rasberry right now!"
-    const funfactsUser2 = "Your baby is 20 weeks old. It's the size of an Artichoke right now!"
+    const funfactsUser1 =
+      "Your baby is 8 weeks old. It's the size of a rasberry right now!";
+    const funfactsUser2 =
+      "Your baby is 20 weeks old. It's the size of an Artichoke right now!";
 
     // const open = Boolean(anchorEl);
     // removing anchorEl from const above as defined and not used
@@ -75,11 +72,7 @@ class MenuAppBar extends React.Component {
     // the find will get me an element without the array. So in render {userFunfacts.text} {userFunfacts.image}
     // console.log("SHOW ME THIS", userFunFacts)
     // const userFunFacts = this.props.currentUser.id ? this.state.funfacts.find(element => element.id === this.props.currentUser.id):null
-    console.log("PLZ SHOW THIS", this.props.currentUser)
-
-
-
-
+    console.log("PLZ SHOW THIS", this.props.currentUser);
 
     return (
       <div className={classes.root}>
@@ -93,14 +86,26 @@ class MenuAppBar extends React.Component {
               <MenuIcon />
             </IconButton> */}
             <div>
-              <img src="./components/images/logo2.png" height="25" width="25" alt="logo" />
+              <img
+                src="./components/images/logo2.png"
+                height="25"
+                width="25"
+                alt="logo"
+              />
             </div>
             <Typography variant="h6" color="inherit" className={classes.grow}>
-              {(this.props.currentUser) ? this.props.currentUser.email : "Not logged in!" }
+              {this.props.currentUser
+                ? this.props.currentUser.email
+                : "Not logged in!"}
               {/* {userFunFacts.email} */}
               {/* {userFunFacts.dueDate} */}
-              <a>  : Your baby is 20 weeks old ! Equivalent to an Artichoke</a>
-              <img src="http://placekitten.com/25/25" alt="Kitten" height="25" width="25" />
+              <a> : Your baby is 20 weeks old ! Equivalent to an Artichoke</a>
+              <img
+                src="http://placekitten.com/25/25"
+                alt="Kitten"
+                height="25"
+                width="25"
+              />
             </Typography>
             <Typography variant="h6" color="inherit" className={classes.grow} />
             {auth && <DrawerRight />}
@@ -116,6 +121,3 @@ MenuAppBar.propTypes = {
 };
 
 export default withStyles(styles)(MenuAppBar);
-
-
-
